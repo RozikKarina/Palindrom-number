68bcadc20c682283390590f23e480eb6e7d21df3
Karina Rozenboym <karinarozik1@gmail.com>
2024-10-27 03:02:52 +0200

    Добавьте файлы проекта.

---
+++ Palindrom-number/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0) return false; // Від'ємне число не може бути паліндромом
+
+        int original = x;
+        int reversed = 0;
+
+        // Повертаємо число в зворотному порядку
+        while (x > 0)
+        {
+            int remainder = x % 10;
+            reversed = reversed * 10 + remainder;
+            x /= 10;
+        }
+
+        // Перевіряємо, чи дорівнює оригінальне число зворотному
+        return original == reversed;
+    }
+}
